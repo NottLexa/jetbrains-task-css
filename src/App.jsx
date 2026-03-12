7105a2a64d66f449990e81be4c1ba2ff7b64f38f
NottLexa <94920046+nottlexa@users.noreply.github.com>
2026-03-12 23:36:10 +0200

    Added additional scss for better understanding of multiple stylesheet files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
     return (
         <div className="container">
             <h1 className="title">Parcel + React + Sass</h1>
-            <button className="button">Click me</button>
+            <button className="button cool-font">Click me</button>
         </div>
     );
 }
