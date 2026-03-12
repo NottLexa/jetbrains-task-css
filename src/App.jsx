5a22f04621350fc911a16f954ac806e5521f9bc3
NottLexa <94920046+nottlexa@users.noreply.github.com>
2026-03-12 23:05:27 +0200

    Parcel project init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className="container">
+            <h1 className="title">Parcel + React + Sass</h1>
+            <button className="button">Click me</button>
+        </div>
+    );
+}
